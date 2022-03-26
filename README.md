126e36b42b29c7f4615af38d1b048e39ae160b83
hala277 <hala.almasharfeh@gmail.com>
2022-03-26 22:25:50 +0300

    add

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Message-app
+
+## chat app
+
+*this app name is shat app which it send message and choose room to send a message also.*
+
+**to create this app i did use**
++ soket.io
++ soket.io-client
++ @socket.io/admin-ui
++ https://admin.socket.io
+
+you can use this app by open the `http://localhost:5500/client/?` in your web by make sure that the server is working.
+
+## UML
+![UML](./img/uml.jpg)
---
+++ client/script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const joinRoomButton = document.getElementById("join-button");
+const messageInput = document.getElementById("message-input");
+const roomInput = document.getElementById("room-input");
+const form = document.getElementById("form");
+
+const socket = io("http://localhost:3000");
+const userSocket = io("http://localhost:3000/user", {
+  auth: { token: "Test" },
+});
+
+socket.on("connect", () => {
+  displayMessage(`"You connected with: ${socket.id}`);
+});
+socket.on("pass-message", (message) => {
+  displayMessage(message);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  const room = roomInput.value;
+
+  if (message === "") return;
+  displayMessage(message);
+  socket.emit("send-message", message, room);
+
+  messageInput.value = "";
+});
+
+joinRoomButton.addEventListener("click", () => {
+  const room = roomInput.value;
+  socket.emit("join-room", room, (message) => {
+    displayMessage(message);
+  });
+});
+// console.log(joinRoomButton)
+
+function displayMessage(message) {
+  const div = document.createElement("div");
+  div.textContent = message;
+  document.querySelector("#message-container").append(div);
+}
+
+let count = 0;
+setInterval(() => {
+  socket.volatile.emit("ping", count++);
+}, 1000);
+
+document.addEventListener("keydown", (e) => {
+  if (e.target.matches("input")) return;
+
+  if (e.key === "c") socket.connect();
+  if (e.key === "d") socket.disconnect();
+});
